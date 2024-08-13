b1395468927f7a310ab09d8ab7d062474c09849e
ghmamadreza2 <ghmamadreza84@gmail.com>
2024-08-13 15:51:49 +0430

    add album name and album desc to albumitem component

--- src/components/AlbumItem.jsx
+++ src/components/AlbumItem.jsx
@@ -4,6 +4,8 @@ const AlbumItem = ({ image, name, desc, id }) => {
   return (
     <div className="min-w-[180px] p-2 px-3 rounded cursor-pointer hover:bg-[#ffffff26]">
       <img className="rounded" src={image} alt="" />
+      <p className="mt-2 mb-1 font-bold">{name}</p>
+      <p className="text-sm text-slate-200">{desc}</p>
     </div>
   );
 };
