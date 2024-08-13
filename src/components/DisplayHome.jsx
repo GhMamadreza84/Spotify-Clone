3bd45702b43efa4339a365ecd0a541f47d5a5b6f
ghmamadreza2 <ghmamadreza84@gmail.com>
2024-08-13 15:55:46 +0430

    i map on album item in display home with albumsdata and show albums with a card in home

--- src/components/DisplayHome.jsx
+++ src/components/DisplayHome.jsx
@@ -1,12 +1,21 @@
 import React from "react";
 import Navbar from "./Navbar";
-
+import { albumsData, assets } from "../assets/assets";
+import AlbumItem from "./AlbumItem";
 const DisplayHome = () => {
   return (
     <>
       <Navbar />
-      <div>
-        
+      <div className="my-5 text-2xl font-bold">
+        {albumsData.map((item, index) => (
+          <AlbumItem
+            key={index}
+            name={item.name}
+            desc={item.desc}
+            id={item.id}
+            image={item.image}
+          />
+        ))}
       </div>
     </>
   );
