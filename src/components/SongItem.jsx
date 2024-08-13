6499cd7973c56b6a3c6a605136f48d4b6010971d
ghmamadreza2 <ghmamadreza84@gmail.com>
2024-08-13 16:15:22 +0430

    i destructure props entries in songItem component

--- src/components/SongItem.jsx
+++ src/components/SongItem.jsx
@@ -1,7 +1,9 @@
 import React from "react";
 
 const SongItem = ({ name, desc, id, image }) => {
-  return <div></div>;
+  return <div className="min-w-[180px] p-2 px-3 rounded">
+    
+  </div>;
 };
 
 export default SongItem;
