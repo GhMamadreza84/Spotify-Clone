5c2242dfc7c79bfd89bfbe0ae22d55add7d6642f
ghmamadreza2 <ghmamadreza84@gmail.com>
2024-08-13 16:02:33 +0430

    i change class name display

--- src/components/Display.jsx
+++ src/components/Display.jsx
@@ -4,7 +4,7 @@ import DisplayHome from "./DisplayHome";
 
 const Display = () => {
   return (
-    <div className="w-[100%] m-2 px-6 pt-4 rounded bg-[#121212] text-white overflow-hidden lg:w-[75%] lg:ml-0">
+    <div className="w-[100%] m-2 px-6 pt-4 rounded bg-[#121212] text-white overflow-auto  lg:w-[75%] lg:ml-0">
       <Routes>
         <Route path="/" element={<DisplayHome />} />
       </Routes>
--- src/components/DisplayHome.jsx
+++ src/components/DisplayHome.jsx
@@ -6,16 +6,19 @@ const DisplayHome = () => {
   return (
     <>
       <Navbar />
-      <div className="my-5 text-2xl font-bold">
-        {albumsData.map((item, index) => (
-          <AlbumItem
-            key={index}
-            name={item.name}
-            desc={item.desc}
-            id={item.id}
-            image={item.image}
-          />
-        ))}
+      <div className="mb-4">
+        <h1 className="my-5 text-2xl font-bold">Featured Charts</h1>
+        <div className="flex overflow-auto ">
+          {albumsData.map((item, index) => (
+            <AlbumItem
+              key={index}
+              name={item.name}
+              desc={item.desc}
+              id={item.id}
+              image={item.image}
+            />
+          ))}
+        </div>
       </div>
     </>
   );
