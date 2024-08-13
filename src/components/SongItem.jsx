03aca4e61b5899a869dcf15fbe15681bca94835c
ghmamadreza2 <ghmamadreza84@gmail.com>
2024-08-13 16:17:23 +0430

    i complate songitem component and now ready for maping

--- src/components/SongItem.jsx
+++ src/components/SongItem.jsx
@@ -1,9 +1,13 @@
 import React from "react";
 
 const SongItem = ({ name, desc, id, image }) => {
-  return <div className="min-w-[180px] p-2 px-3 rounded">
-    
-  </div>;
+  return (
+    <div className="min-w-[180px] p-2 px-3 rounded cursor-pointer hover:bg-[#ffffff26]">
+      <img className="rounded" src={image} alt="" />
+      <p className="mt-2 mb-1 font-bold">{name}</p>
+      <p className="text-sm text-slate-200">{desc}</p>
+    </div>
+  );
 };
 
 export default SongItem;
