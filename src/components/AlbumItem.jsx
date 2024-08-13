0731085276875a6c84ae8845d440e3dcc6270404
ghmamadreza2 <ghmamadreza84@gmail.com>
2024-08-13 15:49:23 +0430

    add class name to albumitem div and add image tag

--- src/components/AlbumItem.jsx
+++ src/components/AlbumItem.jsx
@@ -1,9 +1,11 @@
 import React from "react";
 
 const AlbumItem = ({ image, name, desc, id }) => {
-  return <div className="">
-
-  </div>;
+  return (
+    <div className="min-w-[180px] p-2 px-3 rounded cursor-pointer hover:bg-[#ffffff26]">
+      <img className="rounded" src={image} alt="" />
+    </div>
+  );
 };
 
 export default AlbumItem;
