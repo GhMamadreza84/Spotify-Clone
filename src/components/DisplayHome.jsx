ed5b80268029f46609fd50df1dc5f4af67c2439e
ghmamadreza2 <ghmamadreza84@gmail.com>
2024-08-13 16:07:39 +0430

    i add todays song  hits section

--- src/components/DisplayHome.jsx
+++ src/components/DisplayHome.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import Navbar from "./Navbar";
-import { albumsData, assets } from "../assets/assets";
+import { albumsData, assets, songsData } from "../assets/assets";
 import AlbumItem from "./AlbumItem";
 const DisplayHome = () => {
   return (
@@ -10,6 +10,18 @@ const DisplayHome = () => {
         <h1 className="my-5 text-2xl font-bold">Featured Charts</h1>
         <div className="flex overflow-auto ">
           {albumsData.map((item, index) => (
+            <AlbumItem
+              key={index}
+              name={item.name}
+              desc={item.desc}
+              id={item.id}
+              image={item.image}
+            />
+          ))}
+        </div>
+        <h1 className="my-5 text-2xl font-bold">Today's song hits</h1>
+        <div className="flex overflow-auto ">
+          {songsData.map((item, index) => (
             <AlbumItem
               key={index}
               name={item.name}
