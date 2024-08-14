5d8887e196582b95f46d3f23faae4ac85b46e039
ghmamadreza2 <ghmamadreza84@gmail.com>
2024-08-14 15:35:21 +0430

    i use navigate hook for song albums and items

--- src/components/AlbumItem.jsx
+++ src/components/AlbumItem.jsx
@@ -5,7 +5,7 @@ const AlbumItem = ({ image, name, desc, id }) => {
   const navigate = useNavigate();
   return (
     <div
-      onClick={navigate(`/album/${id}`)}
+      onClick={() => navigate(`/album/${id}`)}
       className="min-w-[180px] p-2 px-3 rounded cursor-pointer hover:bg-[#ffffff26]"
     >
       <img className="rounded" src={image} alt="" />
--- src/components/SongItem.jsx
+++ src/components/SongItem.jsx
@@ -1,8 +1,12 @@
 import React from "react";
-
+import { useNavigate } from "react-router-dom";
 const SongItem = ({ name, desc, id, image }) => {
+  const navigate = useNavigate();
   return (
-    <div className="min-w-[180px] p-2 px-3 rounded cursor-pointer hover:bg-[#ffffff26]">
+    <div
+      onClick={() => navigate(`/song/${id}`)}
+      className="min-w-[180px] p-2 px-3 rounded cursor-pointer hover:bg-[#ffffff26]"
+    >
       <img className="rounded" src={image} alt="" />
       <p className="mt-2 mb-1 font-bold">{name}</p>
       <p className="text-sm text-slate-200">{desc}</p>
