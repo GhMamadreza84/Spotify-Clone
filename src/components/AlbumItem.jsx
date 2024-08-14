f5075fc915e1630aaedf57a9a9c19d60c3031bde
ghmamadreza2 <ghmamadreza84@gmail.com>
2024-08-14 15:32:34 +0430

    i using usenavigate hook from react router dom and i complate operation for when click on the album item opened album item page

--- src/components/AlbumItem.jsx
+++ src/components/AlbumItem.jsx
@@ -1,8 +1,13 @@
 import React from "react";
+import { useNavigate } from "react-router-dom";
 
 const AlbumItem = ({ image, name, desc, id }) => {
+  const navigate = useNavigate();
   return (
-    <div className="min-w-[180px] p-2 px-3 rounded cursor-pointer hover:bg-[#ffffff26]">
+    <div
+      onClick={navigate(`/album/${id}`)}
+      className="min-w-[180px] p-2 px-3 rounded cursor-pointer hover:bg-[#ffffff26]"
+    >
       <img className="rounded" src={image} alt="" />
       <p className="mt-2 mb-1 font-bold">{name}</p>
       <p className="text-sm text-slate-200">{desc}</p>
