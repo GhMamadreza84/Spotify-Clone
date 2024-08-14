4ef59ba7e50618610e02b80d1e2d0eb3c21bf0f7
ghmamadreza2 <ghmamadreza84@gmail.com>
2024-08-14 15:44:14 +0430

    i use params hook in display album for get album id

--- src/components/DisplayAlbum.jsx
+++ src/components/DisplayAlbum.jsx
@@ -1,11 +1,15 @@
-import React from 'react'
+import React from "react";
+import Navbar from "./Navbar";
+import { useParams } from "react-router-dom";
 
 const DisplayAlbum = () => {
+  const { id } = useParams();
+  console.log(id);
   return (
     <>
-      
+      <Navbar />
     </>
-  )
-}
+  );
+};
 
-export default DisplayAlbum
+export default DisplayAlbum;
