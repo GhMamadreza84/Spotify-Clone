42a3b57def68dcdd881a3a317fb2252154e96dc4
ghmamadreza2 <ghmamadreza84@gmail.com>
2024-08-16 23:07:07 +0430

    i wrap my player context provider around the app component

--- src/main.jsx
+++ src/main.jsx
@@ -3,10 +3,13 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import App from "./App.jsx";
 import "./index.css";
+import PlayerContextProvider from "./context/PlayerContext.jsx";
 createRoot(document.getElementById("root")).render(
   <StrictMode>
     <BrowserRouter>
-      <App />
+      <PlayerContextProvider>
+        <App />
+      </PlayerContextProvider>
     </BrowserRouter>
   </StrictMode>
 );
