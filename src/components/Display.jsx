f7245f9f5b5e2a63e16e12f8fb3cf4d18570ca0c
ghmamadreza2 <ghmamadreza84@gmail.com>
2024-08-16 22:25:35 +0430

    import useref hook in display component and sync with main div

--- src/components/Display.jsx
+++ src/components/Display.jsx
@@ -1,11 +1,16 @@
-import React from "react";
+import React, { useRef } from "react";
 import { Route, Routes } from "react-router-dom";
 import DisplayHome from "./DisplayHome";
 import DisplayAlbum from "./DisplayAlbum";
 
+const displayRef = useRef();
+
 const Display = () => {
   return (
-    <div className="w-[100%] m-2 px-6 pt-4 rounded bg-[#121212] text-white overflow-auto  lg:w-[75%] lg:ml-0">
+    <div
+      ref={displayRef}
+      className="w-[100%] m-2 px-6 pt-4 rounded bg-[#121212] text-white overflow-auto  lg:w-[75%] lg:ml-0"
+    >
       <Routes>
         <Route path="/" element={<DisplayHome />} />
         <Route path="/album/:id" element={<DisplayAlbum />} />
