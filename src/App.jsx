ddda0d5adc35c1d787ef50f0e1c62c51d37bd76d
ghmamadreza2 <ghmamadreza84@gmail.com>
2024-08-18 15:58:35 +0430

    i destructure track state in app component and use in audio tag for src

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,7 @@ import Player from "./components/Player";
 import Display from "./components/Display";
 
 const App = () => {
-  const { audioRef } = useContext(PlayerContext);
+  const { audioRef, track } = useContext(PlayerContext);
 
   return (
     <div className="h-screen bg-black">
@@ -14,7 +14,7 @@ const App = () => {
         <Display />
       </div>
       <Player />
-      <audio ref={audioRef} preload="auto"></audio>
+      <audio ref={audioRef} src={track.file} preload="auto"></audio>
     </div>
   );
 };
